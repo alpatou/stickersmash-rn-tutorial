30758c1118dcc60e3e85da323143cc247a35c4b5
l l <l@l.l>
2023-12-28 17:31:35 +0200

    pan gersture

--- EmojiSticker.js
+++ EmojiSticker.js
@@ -4,8 +4,29 @@ import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
 
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
     const scaleImage = useSharedValue(imageSize);
 
+    const drag = Gesture.Pan().onChange((event) => {
+        translateX.value += event.changeX;
+        translateY.value += event.changeY;
+    });
+
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value
+                },
+                {
+                    translateY: translateY.value
+                }
+            ]
+        };
+    });
 
     const doubleTap = Gesture.Tap().numberOfTaps(2).onStart(() => {
         if (scaleImage.value !== imageSize * 2) {
@@ -21,14 +42,14 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     });
 
     return (
-        <View style={{ top: -350 }}>
-            <GestureDetector gesture={doubleTap}>
+        <GestureDetector gesture={drag}>
+            <Animated.View style={{ containerStyle, top: -350 }}>
                 <Animated.Image
                     source={stickerSource}
                     resizeMode="contain"
                     style={[imageStyle, { width: imageSize, height: imageSize }]}
                 />
-            </GestureDetector>
-        </View>
+            </Animated.View>
+        </GestureDetector>
     );
 }
